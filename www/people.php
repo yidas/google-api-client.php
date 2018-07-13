fd66ff64704b216b94c6696b1a359da7efb0c7af
Nick Tsai <myintaer@gmail.com>
2018-07-13 14:46:03 +0800

    Finish people contact

--- www/people.php
+++ www/people.php
@@ -30,38 +30,132 @@ if ($client->isAccessTokenExpired()) {
 $service = new Google_Service_PeopleService($client);
 
 try {
+
+    $op = isset($_GET['op']) ? $_GET['op'] : null;
     
-    
-    // Print the names for up to 10 connections.
-    $optParams = array(
-        'pageSize' => 0,
-        'personFields' => 'names,emailAddresses,phoneNumbers',
-    );
-	$results = $service->people_connections->listPeopleConnections('people/me', $optParams);
-	
-	// Parser
-	$contacts = [];
-    
-    if (count($results->getConnections()) != 0) {
+    switch ($op) {
+        case 'add':
+        case 'update':
+            
+            $name = isset($_POST['name']) ? $_POST['name'] : '';
+            $email = isset($_POST['email']) ? $_POST['email'] : '';
+            $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+            $resourceName = isset($_POST['id']) ? $_POST['id'] : null;
+            $editMode = ($resourceName) ? true : false; 
+
+            // Get person from API search to simple develop likes preventing eTag check
+            $person = ($editMode) 
+                ? $service->people->get($resourceName)
+                : new Google_Service_PeopleService_Person;
+            // var_dump($person);exit;
+
+            // UpdatePersonFields mask is required for update
+            $updatePersonFields = [];
+            
+            // Name
+            $peopleName = new Google_Service_PeopleService_Name;
+            if ($name) {
+                // Add updatePersonFields
+                $updatePersonFields[] = 'names';
+
+                // First name
+                $peopleName->setGivenName($name);
+                    // Middle name
+                $peopleName->setMiddleName('');
+                // Last name
+                $peopleName->setFamilyName('');
+                $person->setNames($peopleName);
+            }
+            
+            // eMail
+            if ($email) {
+                // Add updatePersonFields
+                $updatePersonFields[] = 'emailAddresses';
+
+                $emailAddress = new Google_Service_PeopleService_EmailAddress;
+                $emailAddress->setValue($email);
+                $person->setEmailAddresses($emailAddress);
+            }
+            
+            // Phone
+            if ($phone) {
+                // Add updatePersonFields
+                $updatePersonFields[] = 'phoneNumbers';
+
+                $phoneNumber = new Google_Service_PeopleService_PhoneNumber;
+                $phoneNumber->setValue($phone);
+                $person->setPhoneNumbers($phoneNumber);
+            }
+            
+            // print_r($person);exit;
+
+            if ($editMode) {
+                
+                $result = $service->people->updateContact($resourceName, $person, [
+                    'updatePersonFields' => $updatePersonFields
+                ]);
+
+            } else {
+
+                $result = $service->people->createContact($person);
+            }
+
+            // Back to default route of this function 
+            header("Location: {$_SERVER['PHP_SELF']}");
+            exit;
+            break;
+
+        case 'delete':
+            
+            $resourceName = isset($_GET['id']) ? $_GET['id'] : null;
+
+            if ($resourceName) {
+                
+                $result = $result = $service->people->deleteContact($resourceName);
+                // var_dump($result);exit;
+            }
+            
+            // Back to default route of this function 
+            header("Location: {$_SERVER['PHP_SELF']}");
+            exit;
+            break;
         
-        foreach ($results->getConnections() as $person) {
+        default:
 
-			$data = [];
-            $data['name'] = isset($person->getNames()[0]) 
-                ? $person->getNames()[0]->getDisplayName() 
-                : null;
-            $data['email'] = isset($person->getEmailAddresses()[0]) 
-                ? $person->getEmailAddresses()[0]->getValue() 
-                : null;
-            $data['phone'] = isset($person->getPhoneNumbers()[0]) 
-                ? $person->getPhoneNumbers()[0]->getValue() 
-                : null;
-            // print_r($data['email']);
+            // Print the names for up to 10 connections.
+            $optParams = array(
+                'pageSize' => 0,
+                'personFields' => 'names,emailAddresses,phoneNumbers',
+            );
+            $results = $service->people_connections->listPeopleConnections('people/me', $optParams);
+            
+            // Parser
+            $contacts = [];
+            
+            if (count($results->getConnections()) != 0) {
+                
+                foreach ($results->getConnections() as $person) {
 
-			$contacts[] = $data;
-        }
-	}
-	// print_r($contacts);exit;
+                    // var_dump($person);exit;
+                    $data = [];
+                    // Resource name
+                    $data['id'] = $person->getResourceName();
+                    $data['name'] = isset($person->getNames()[0]) 
+                        ? $person->getNames()[0]->getDisplayName() 
+                        : null;
+                    $data['email'] = isset($person->getEmailAddresses()[0]) 
+                        ? $person->getEmailAddresses()[0]->getValue() 
+                        : null;
+                    $data['phone'] = isset($person->getPhoneNumbers()[0]) 
+                        ? $person->getPhoneNumbers()[0]->getValue() 
+                        : null;
+
+                    $contacts[] = $data;
+                }
+            }
+            // print_r($contacts);exit;
+            break;
+    }
     
 } catch (Exception $e) {
 
@@ -91,13 +185,16 @@ try {
 </head>
 <body>
 
-<h3><a href="./">Google API</a> - Drive</h3>
+<h3><a href="./">Google API</a> - People</h3>
 
 <form action="?op=add" method="post">
+<input type="submit" value="Save" name="submit">
+ID: <input type="text" name="id" size="25" placeholder="For Update" />
+<input type="reset" value="Reset">
+<br><br>
 Name: <input type="text" name="name" size="10" />
 eMail: <input type="text" name="email" size="20" />
 Phone: <input type="text" name="phone" size="10" />
-<input type="submit" value="Add" name="submit">
 </form>
 
 <hr/>
@@ -112,15 +209,27 @@ Phone: <input type="text" name="phone" size="10" />
   </thead>
   <?php foreach ($contacts as $key => $contact): ?>
   <tr>
-    <td><?=$key?></td>
+    <td><?=$key+1?></td>
     <td><?=$contact['name']?></td>
     <td><?=$contact['email']?></td>
     <td><?=$contact['phone']?></td>
-    <td></td>
+    <td>
+      <a href="javascript:edit('<?=$contact['id']?>', '<?=$contact['name']?>', '<?=$contact['email']?>', '<?=$contact['phone']?>')">Edit</a>
+      <a href="javascript:if(confirm('Confirm delete?')){location.href='?op=delete&id=<?=$contact['id']?>'}">Delete</a>
+    </td>
   </tr>
   <?php endforeach ?>
 </table>
 
+<script>
+  function edit(id, name, email, phone) {
+    document.getElementsByName("id")[0].value = id;
+    document.getElementsByName("name")[0].value = name;
+    document.getElementsByName("email")[0].value = email;
+    document.getElementsByName("phone")[0].value = phone;
+    document.getElementsByName("submit")[0].focus();
+  }
+</script>
 
 </body>
 </html>
