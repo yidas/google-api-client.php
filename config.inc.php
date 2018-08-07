cf9f935c336a3099663204728380a33fbfed2efd
Nick Tsai <myintaer@gmail.com>
2018-08-07 16:58:24 +0800

    Fix config

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'redirectUri' => "http://{$_SERVER['HTTP_HOST']}" . dirname($_SERVER['PHP_SELF']) . "/callback.php",
+    'redirectUri' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}" . dirname($_SERVER['PHP_SELF']) . "/callback.php",
     'authConfig' => __DIR__ . '/files/google_api_secret.json',
     // Google Service Scopes
     'serviceScopes' => [
